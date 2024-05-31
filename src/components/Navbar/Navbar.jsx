c3084466a7c498f2ece69f7b28c981041891c734
Wilder Ap <wilderasprilla321@gmail.com>
2024-05-31 09:29:41 -0500

    copys logos and texts modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import data from "../../data.json";
 
-
 // import { HiMenu, HiX } from "react-icons/hi";
 
 function Navbar() {
@@ -12,25 +11,24 @@ function Navbar() {
   };
 
   return (
-    <div className="max-w-full fixed top-0 w-full h-min-content"
-    style={{ backgroundColor: "white", zIndex: 100 }}>
+    <div
+      className="max-w-full fixed top-0 w-full h-min-content"
+      style={{ backgroundColor: "white", zIndex: 100 }}
+    >
       <header>
         <nav className="flex justify-between px-7 py-6 sm:p-6  items-center w-full position: fixed bg-[white] p-4 fixed top-0 left-0 w-full z-5">
           <a href="/">
             <div className="flex items-center gap- pr-4">
               <img
-                height={32}
-                width={32}
+                height={132}
+                width={132}
                 src={data.sections.header.logo}
                 alt={data.sections.header.alt}
               />
-              <p className="text-black">
-                {data.sections.header.text_logo}
-              </p>
+              <p className="text-black">{data.sections.header.text_logo}</p>
             </div>
           </a>
           <ul className="items-center justify-around hidden sm:flex md:gap-2 lg:gap-8 xl:gap-12 mr-80">
-            
             {data.sections.header.navbar.map((item, index) => (
               <a
                 href={item.link}
@@ -43,7 +41,7 @@ function Navbar() {
               </a>
             ))}
           </ul>
-          
+
           <div className="items-center text-lg hidden sm:flex gap-6 min-w-max-content px-4">
             <a
               href="#"
@@ -55,22 +53,27 @@ function Navbar() {
           <input type="text" className="text-white bg-black rounded-md" placeholder="Search DOML" />
          </div> */}
             <button className="bg-white text-black py-2 px-4 rounded whitespace-nowrap">
-              <p className="font-bold text-[14px] md:text-[px] lg:text-[18px]">Sign in
+              <p className="font-bold text-[14px] md:text-[px] lg:text-[18px]">
+                Sign in
                 {data.sections.header.userSignUp}
               </p>
             </button>
             <button className="bg-white text-black py-2 px-4 rounded border border-black whitespace-nowrap">
-              <p className="font-bold text-[14px] md:text-[16px] lg:text-[18px]">Sign up
+              <p className="font-bold text-[14px] md:text-[16px] lg:text-[18px]">
+                Sign up
                 {data.sections.header.userSignUp}
               </p>
             </button>
-            
           </div>
           <div className="sm:hidden">
             {isMenuOpen ? (
               <div className="relative">
-                <div onClick={handleMenuOpen}
-                 className="text-primary w-8 h-8 cursor-pointer">////</div>
+                <div
+                  onClick={handleMenuOpen}
+                  className="text-primary w-8 h-8 cursor-pointer"
+                >
+                  ////
+                </div>
                 <div className="absolute z-40 right-0 bg-green_light bg-white px-6 rounded-sm shadow-xl">
                   <ul className="">
                     {data.sections.header.navbar.map((item, index) => (
@@ -100,8 +103,12 @@ function Navbar() {
               //   onClick={handleMenuOpen}
               //   className="text-primary w-8 h-8 cursor-pointer"
               // />
-              <div onClick={handleMenuOpen}
-                 className="text-primary w-8 h-8 cursor-pointer">////</div>
+              <div
+                onClick={handleMenuOpen}
+                className="text-primary w-8 h-8 cursor-pointer"
+              >
+                ////
+              </div>
             )}
           </div>
         </nav>
--- src/components/Sections/FiveSection.jsx
+++ src/components/Sections/FiveSection.jsx
@@ -5,9 +5,9 @@ function FiveSection() {
   const words = data.sections.seven.title.split(" ");
 
   return (
-    <div id="pricing" className="relative ">
+    <div className="relative">
       <div className="grid sm:grid-cols-2 gap-2 mt-20">
-        <div className="text-center">
+        <div className="flex flex-col justify-center items-start text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold">
           <h2 style={{ lineHeight: "1.2" }}>
             {words.map((word, index) => (
               <span
@@ -20,27 +20,63 @@ function FiveSection() {
               </span>
             ))}
           </h2>
-          <p className="text-black text-xs mt-7">{data.sections.seven.subtitule}</p>
-          <p className="text-black text-xs mt-7">{data.sections.seven.subtitule1}</p>
-          <p className="text-black text-xs mt-7">{data.sections.seven.subtitule2}</p>
+          <p className="text-black text-xs mt-7">
+            {data.sections.nine.subtitule}
+          </p>
           <div>
-         </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10 text-xs">
+              Learn More
+            </button>
+          </div>
           <h2 className="text-[#080808] sm:mb-60"></h2>
           <img
-            src={data.sections.seven.image1}
-            alt={data.sections.seven.alt1}
+            src={data.sections.nine.image3}
+            alt={data.sections.nine.alt1}
             width="100%"
             height="auto"
             className="block sm:hidden"
           />
         </div>
         <div className="hidden sm:block">
-          <img src={data.sections.seven.image1} alt={data.sections.seven.alt1} />
+          <img src={data.sections.nine.image3} alt={data.sections.nine.alt1} />
         </div>
       </div>
     </div>
+    // <div id="pricing" className="relative ">
+    //   <div className="grid sm:grid-cols-2 gap-2 mt-20">
+    //     <div className="text-center">
+    //       <h2 style={{ lineHeight: "1.2" }}>
+    //         {words.map((word, index) => (
+    //           <span
+    //             key={index}
+    //             style={{
+    //               color: word === "expertise" ? "#080808" : "inherit",
+    //             }}
+    //           >
+    //             {word}{" "}
+    //           </span>
+    //         ))}
+    //       </h2>
+    //       <p className="text-black text-xs mt-7">{data.sections.seven.subtitule}</p>
+    //       <p className="text-black text-xs mt-7">{data.sections.seven.subtitule1}</p>
+    //       <p className="text-black text-xs mt-7">{data.sections.seven.subtitule2}</p>
+    //       <div>
+    //      </div>
+    //       <h2 className="text-[#080808] sm:mb-60"></h2>
+    //       <img
+    //         src={data.sections.seven.image1}
+    //         alt={data.sections.seven.alt1}
+    //         width="100%"
+    //         height="auto"
+    //         className="block sm:hidden"
+    //       />
+    //     </div>
+    //     <div className="hidden sm:block">
+    //       <img src={data.sections.seven.image1} alt={data.sections.seven.alt1} />
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
 export default FiveSection;
-          
