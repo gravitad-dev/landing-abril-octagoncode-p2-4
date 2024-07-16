4c8ce6d5c00278a4a132f0daf5ee1d0f87f9c031
Wilder Ap <wilderasprilla321@gmail.com>
2024-07-16 15:36:44 -0500

    Social buttons

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -4,16 +4,20 @@ import Hero from "../Hero/Hero";
 import Layout from "../Layout/Layout";
 import Body from "../Body/Body";
 import Footer from "../Footer/Footer";
+import SocialButtons from "../Socials";
 
 function LandingPage() {
   return (
     <div>
       <Navbar />
       <Layout>
-        <section id="product"><Hero /></section>
+        <section id="product">
+          <Hero />
+        </section>
         <Body />
       </Layout>
       <Footer />
+      <SocialButtons />
     </div>
   );
 }
